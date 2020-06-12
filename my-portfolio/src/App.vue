cb052525129556d1ba436b0db859278e270e2785
sully34 <sssulai@gmail.com>
2020-06-12 18:07:41 -0400

    first commit

---
+++ my-portfolio/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="main-wrapper">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'My portfolio'
+    }
+  }
+}
+</script>
+
+<style>
